01c24225b90b6dd638bdf53b6e93efbcc8dd6fdf
Alexandros Nikolaos Ziogas <alexandros.ziogas@inf.ethz.ch>
2023-10-31 20:32:22 +0100

    Added efficiency script.

---
+++ efficiency.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+v100_df = pd.read_csv('energy_df_v100_total.csv')
+a100_df = pd.read_csv('energy_df_a100_total.csv')
+
+print(v100_df.groupby(['M']).sum())
+print(a100_df.groupby(['M']).sum())
